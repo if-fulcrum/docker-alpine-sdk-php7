14d019356799ed959a890109eb9cec4baae51610
Kenn Herman <kenn@ifsight.com>
2018-07-20 16:53:48 -0700

    change ver, chown, add -k

--- bump-php.sh
+++ bump-php.sh
@@ -3,15 +3,17 @@
 # This is to be volume mounted into an alpine docker box for building
 
 if [ $# -ne 1 ]; then
-  echo "Usage: $0 7.1.13"
+  echo "Usage: $0 7.1.21"
   exit 1
 else
   PHPVERSION=$1
 fi
+
+sudo chown -R $(whoami) ~/aports 
 
 # make sure we are up to date
 cd ~/aports && git pull
 
 # we want to abump php7
 echo "Bumping PHP to $PHPVERSION"
-cd ~/aports/community/php7 && abump php7-$PHPVERSION
+cd ~/aports/community/php7 && abump -k php7-$PHPVERSION
